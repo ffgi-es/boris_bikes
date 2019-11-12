389f094211bc73d9571ba4ace51ff8bc439cfc80
Alastair Gilles <agilles91@gmail.com>
2019-11-12 17:48:59 +0100

    added functionality to check and dock a bike into a docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,17 @@
 require "bike"
 
 class DockingStation
+  attr_reader :bike
+
   def release_bike
     Bike.new
+  end
+
+  def dock_bike bike
+    @bike = bike
+  end
+
+  def docked?
+    !self.bike.nil?
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,6 +7,7 @@ describe DockingStation do
   end
 
   it {is_expected.to respond_to :release_bike}
+  
   describe "release_bike" do
     it "should create an instance of bike" do
       expect(subject.release_bike).to be_instance_of Bike
@@ -15,10 +16,31 @@ describe DockingStation do
       bike = subject.release_bike
       expect(bike.working?).to eq true
     end
-    
   end
 
-  
+  it { is_expected.to respond_to :dock_bike }
+
+  describe "#dock_bike" do
+    it "should accept a Bike" do
+      bike = Bike.new
+      expect{ subject.dock_bike bike }.to_not raise_error
+    end
+  end
+
+  it { is_expected.to have_attributes(bike: nil) }
+  it { is_expected.to respond_to(:docked?) }
+
+  describe "#docked?" do
+    it "should tell a user if it is empty" do
+      expect(subject.docked?).to eq false
+    end
+
+    it "should tell a user if it is empty" do
+      bike = Bike.new
+      subject.dock_bike bike
+      expect(subject.docked?).to eq true
+    end
+  end
 end
 
 
